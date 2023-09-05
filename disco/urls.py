30788b9cf2754c1c97aaea21090c7192021e20cc
Cyrus Hiatt <chiatt@fargeo.com>
2023-09-05 05:07:37 -0700

    initial addition of files

---
+++ disco/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, re_path, path
+
+
+urlpatterns = [
+    re_path(r"^", include("arches.urls")),
+    re_path(r"^", include("afs.urls")),
+    path("reports/", include("arches_templating.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
