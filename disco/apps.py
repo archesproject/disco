8eba4fc07815d59171e7f6a569f726dcccf5364b
Cyrus Hiatt <chiatt@fargeo.com>
2024-08-02 08:39:04 -0700

    Update apps.py for 7.6 latest

--- disco/apps.py
+++ disco/apps.py
@@ -1,7 +1,12 @@
 from django.apps import AppConfig
 
+from arches.settings_utils import generate_frontend_configuration
+
 
 class DiscoConfig(AppConfig):
     name = "disco"
     verbose_name = "Disco"
     is_arches_application = True
+
+    def ready(self):
+        generate_frontend_configuration()
