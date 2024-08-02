fef56ba26dadf66431f6a84560b6361d8f2f699e
Cyrus Hiatt <chiatt@fargeo.com>
2024-08-02 09:34:25 -0700

    Pin arches and afs dependencies to branches

--- disco/urls.py
+++ disco/urls.py
@@ -7,6 +7,7 @@ from django.urls import include, re_path, path
 urlpatterns = [
     re_path(r"^", include("arches.urls")),
     re_path(r"^", include("arches_for_science.urls")),
+    # re_path(r"^", include("dashboard.urls")),
     path("reports/", include("arches_templating.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
