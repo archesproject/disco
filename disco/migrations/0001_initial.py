66283e86da22f3d437af9bf59751e579b513fa2a
Cyrus Hiatt <chiatt@fargeo.com>
2024-08-02 17:35:33 -0700

    Blacken

--- disco/migrations/0001_initial.py
+++ disco/migrations/0001_initial.py
@@ -6,6 +6,7 @@ from django.core.files import File
 from django.conf import settings
 import os
 
+
 class Migration(migrations.Migration):
 
     initial = True
@@ -22,10 +23,10 @@ class Migration(migrations.Migration):
             name="Example Template",
             description="This is an example template",
             defaults={
-                "template":"document_templates/example-template.docx",
-                "preview":"document_templates/72cc4dcf-9500-418f-a42e-7d980937a9db_preview.pdf",
-                "thumbnail":"document_templates/72cc4dcf-9500-418f-a42e-7d980937a9db_thumbnail.png",
-                }
+                "template": "document_templates/example-template.docx",
+                "preview": "document_templates/72cc4dcf-9500-418f-a42e-7d980937a9db_preview.pdf",
+                "thumbnail": "document_templates/72cc4dcf-9500-418f-a42e-7d980937a9db_thumbnail.png",
+            },
         )
 
         file_names = (
@@ -35,13 +36,19 @@ class Migration(migrations.Migration):
         )
 
         for name in file_names:
-            with open(os.path.join(settings.APP_NAME, 'document_templates', name), 'rb') as template_file:
-                default_storage.save(os.path.join('document_templates', name), File(template_file))
+            with open(
+                os.path.join(settings.APP_NAME, "document_templates", name), "rb"
+            ) as template_file:
+                default_storage.save(
+                    os.path.join("document_templates", name), File(template_file)
+                )
 
     def remove_document_templates(apps, schema_editor):
         ArchesTemplate = apps.get_model("arches_templating", "ArchesTemplate")
 
-        example_template = ArchesTemplate.objects.get(templateid="72cc4dcf-9500-418f-a42e-7d980937a9db")
+        example_template = ArchesTemplate.objects.get(
+            templateid="72cc4dcf-9500-418f-a42e-7d980937a9db"
+        )
         example_template.delete()
 
     operations = [
