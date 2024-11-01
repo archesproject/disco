cb0d875260a8d29fbc9fd920e63c2ccef1bd4f95
Cyrus Hiatt <chiatt@fargeo.com>
2024-11-01 14:45:54 -0700

    Update to work with Arches 7.6.2 (#40)
    * Upgrade to support latest in arches 76x
    
    * Do not require extension on uppy dependency
    
    * Updates for app config changes
    
    * arches should be greater than 7.6.0a0
    
    * Update to support latest changes for 7.6 projects
    
    * Update to latest project configs
    
    * Update for latest webpack changes
    
    * Update workflows with latest config
    
    * Update apps.py for 7.6 latest
    
    * Update gh action
    
    * Pin arches and afs dependencies to branches
    
    * Cleanup
    
    * Ignore archestemp
    
    * Add dev dependencies to .toml
    
    * Update afs dependency branch
    
    * Blacken
    
    * Pin uppy version
    
    * Fix prettier command
    
    * Add minimal test files
    
    * Remove unnecessary __init__.py
    
    * Re-run updateproject with latest dev/7.6.x
    
    * Update package.json
    
    * Update apps.py for 7.6. latest (#39)
    
    * Update apps.py for 7.6. latest
    
    * Use the arches test runner instead of base test
    
    * Formatting
    
    * Update to require afs 2.0 and Arches 7.6.2
    
    ---------
    
    Co-authored-by: Aaron Gundel <aaron.gundel@gmail.com>

---
+++ disco/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+from arches.settings_utils import generate_frontend_configuration
+
+
+class DiscoConfig(AppConfig):
+    name = "disco"
+    verbose_name = "Disco"
+    is_arches_application = True
+
+    def ready(self):
+        if settings.APP_NAME.lower() == self.name:
+            generate_frontend_configuration()
